283627734d5e39280d51b359cd5dca721482b1f7
Akash Kumar <akashkumarmahto2006@gmail.com>
2025-03-30 19:09:51 +0530

    font change

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,15 +30,19 @@ export default function Header() {
           path: "/contact",
         },
       ]);
+      const [menuToggle, setMenuToggle] = useState('hidden')
+      const menuHandler= ()=>{
+        setMenuToggle('block ')
+      }
     return(
         <div>
             <div className="container mx-auto px-4 lg:px-5 py-5 flex justify-between items-center">
-                <Link href="/"><h2 className="text-sm lg:text-2xl ">Drishti Infotech</h2></Link>
+                <Link href="/"><h2 className="text-sm lg:text-2xl text-blue-800 ">Drishti Infotech</h2></Link>
                 <ul className="flex gap-7 items-center">
-                <RxHamburgerMenu className="text-2xl  lg:hidden"/>
+                <RxHamburgerMenu className="text-2xl  lg:hidden" onClick={menuHandler}/>
                 {navItem.map((e,i)=>{
                     return(
-                        <li className="hidden lg:block" key={i}><Link href={e.path}>{e.name}</Link></li>
+                        <li className={`hidden lg:block`} key={i}><Link href={e.path}>{e.name}</Link></li>
                     )
                 })}
                 </ul>
