61238261601a925a508a4346a3267c88750eb413
Akash Kumar <akashkumarmahto2006@gmail.com>
2025-03-30 07:46:35 +0530

    add-courses

--- src/components/PopularCourses.js
+++ src/components/PopularCourses.js
@@ -1,9 +1,60 @@
+import { useState } from "react"
+
 export default function PopularCourses() {
+    const [courseCard, setCourseCard] = useState([
+        {
+            pic: "",
+            name: "DCA",
+            duration: "3 Months",
+        },
+        {
+            pic: "",
+            name: "Tally",
+            duration: "3 Months",
+        },
+        {
+            pic:"",
+            name:"DTP",
+            duration:"4 Months",
+        },
+        {
+            pic:"",
+            name:"ADCA",
+            duration:"12 Months"
+        },
+        {
+            pic:"",
+            name:"Web Development",
+            duration:"6 Months",
+        },
+        {
+            pic:"",
+            name:"Graphic Designing",
+            duration:"4 Months",
+        },
+        {
+            pic:"",
+            name:"Video Editing",
+            duration:"3 Months",
+        }
+    ])
 
     return(
         <div>
             <div className="container mx-auto px-5 py-5">
-                <h2>Popular Courses</h2>
+                <h2 className="text-4xl font-medium">Popular Courses</h2>
+                <div className="flex flex-wrap justify-between gap-5">
+                    {courseCard.map((e, i)=>{
+                        return(
+                            <div className=" border rounded mt-5">
+                    <img className="w-[200px] h-[150px] border rounded  " src={e.pic} alt={e.name} />
+                    <h4 className="mt-3 text-xl ms-2 font-medium">{e.name}</h4>
+                    <h5 className="text-sm text-slate-500 ms-2">{e.duration}</h5>
+                </div>
+                        )
+                    })}
+                </div>
+               
             </div>
         </div>
     )
